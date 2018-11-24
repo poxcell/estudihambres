be92353f83c6b2aae1b0a5205d745723737f6985
MANUEL\manuel perez <topo2210@hotmail.com>
2018-11-23 20:20:01 -0600

    inicio

---
+++ 3er parcial/Assets/menusYUI/Finales/Controles/backControles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backControles : MonoBehaviour {
+
+	public GameObject inicio;
+		
+	void Update () {
+		if (Input.GetButtonDown("Cancel"))
+		{
+			this.gameObject.SetActive(false);
+			inicio.SetActive(true);
+			
+		}
+		
+	}
+}
---
+++ 3er parcial/Assets/menusYUI/Finales/Menu start/startfade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startfade : MonoBehaviour {
+	private Animator animator;
+	// Use this for initialization
+	void Awake() {
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Submit"))
+		{
+			animator.SetBool("StartFade", true);
+		}
+	}
+}
