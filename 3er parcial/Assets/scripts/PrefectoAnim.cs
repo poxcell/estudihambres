b9dd5799d85b4b91c5b7ac99b789e8cee5879b18
MANUEL\manuel perez <topo2210@hotmail.com>
2018-11-26 13:48:10 -0600

    prefecto ai

---
+++ 3er parcial/Assets/scripts/PrefectoAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PrefectoAnim : MonoBehaviour {
+	private Animator animator;
+	private NavMeshAgent navAgent;
+	public float speed;
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponentInChildren<Animator>();
+		navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = navAgent.velocity.magnitude;
+		animator.SetFloat("speed", speed);
+	}
+	public void Golpear()
+	{
+		animator.SetTrigger("Golpe");
+	}
+}
---
+++ 3er parcial/Assets/scripts/cajaprefecto.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cajaprefecto : MonoBehaviour
+{
+	public float daño;
+	private float tiempo;
+	public float wait;
+	void Update()
+	{
+		if ( tiempo < wait)
+		{
+			tiempo += Time.deltaTime;
+		}
+	}
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			if (tiempo >= wait)
+			{
+
+			GetComponentInParent<PrefectoAnim>().Golpear();
+			other.gameObject.GetComponent<Stats>().tomarDaño(daño);
+				tiempo = 0;
+			}
+		}
+	}
+}
